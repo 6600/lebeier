5e868e689b33d0e8653df8e794d736d2211f585d
PUGE <100284685@qq.com>
2018-09-26 20:56:00 +0800

    完成旋转

--- pages/Music/Music.js
+++ pages/Music/Music.js
@@ -6,6 +6,9 @@ Page({
   data: {
     flag: true,
     Crumbs: '',
+    rotation: 360,
+    isPaused: false,
+    enlargeItem: 10
   },
   SongJump: function () {
     wx.navigateTo({
@@ -24,13 +27,26 @@ Page({
       flag: true
     })
   },
+  animate: function () {
+    if (!this.data.isPaused) {
+      if (this.data.rotation > 360) this.data.rotation = 0
+      if (this.data.rotation < 0) this.data.rotation = 360
+      // this.data.rotation -= 50
+      this.setData({
+        rotation: this.data.rotation - 1
+      })
+      // console.log(this.data.rotation)
+    }
+    requestAnimationFrame(this.animate)
+  },
   //获取跳转参数
   onLoad: function(option) {
-    console.log(option.title)
-    this.data.Crumbs = option.title;
-    var that = this;
-    that.setData({
-    Crumbs:option.title
-    })
+    // console.log(option.title)
+    // this.data.Crumbs = option.title;
+    // var that = this;
+    // that.setData({
+    //   Crumbs:option.title
+    // })
+    this.animate()
   }
 })
